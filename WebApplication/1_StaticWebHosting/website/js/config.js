a07578cc01b8d41c0b4d008452eef65acc3bd77e
harshitha2481 <61981163+harshitha2481@users.noreply.github.com>
2020-04-12 18:23:27 +0530

    Update config.js

--- WebApplication/1_StaticWebHosting/website/js/config.js
+++ WebApplication/1_StaticWebHosting/website/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_pkPj5oeA7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '19csem47temc6iisb9hs42tgke', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'http://wildrydes-apsouth.s3-website.ap-south-1.amazonaws.com' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
     }
 };
